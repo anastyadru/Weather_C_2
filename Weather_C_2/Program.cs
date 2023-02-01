ecc571b2b514106402bd606851f14b15393262a7
anastya_dru <adruts@mail.ru>
2023-02-01 20:12:04 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Program.cs
+++ Weather_C_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
 
 namespace Weather_C_2
 {
@@ -9,6 +10,8 @@ namespace Weather_C_2
         public static void Main(string[] args)
         {
             string url = "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid={API key}";
+
+            HttpWebRequest HttpWebRequest = (HttpWebRequest)WebRequest.Create(url);
         }
     }
 }
