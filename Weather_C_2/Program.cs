9b0b0b826b3a45b1bd5dc728854631ba0f2a1616
anastya_dru <adruts@mail.ru>
2023-02-18 17:22:52 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Program.cs
+++ Weather_C_2/Program.cs
@@ -14,34 +14,13 @@ namespace Weather_C_2
             string city = Console.ReadLine();
             
             string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=1a5442d5206cd9ef2bfc21e5ca523b75";
-
-            // Отправка запроса и получение ответа
-            WebRequest request = WebRequest.Create(url);
+            
+            WebRequest request = WebRequest.Create(url); // Отправила запрос и получила ответ
             WebResponse response = request.GetResponse();
             
-            // Чтение данных из ответа
-            Stream dataStream = response.GetResponseStream();
+            Stream dataStream = response.GetResponseStream(); // Считала данные из ответа
             StreamReader reader = new StreamReader(dataStream);
             string responseFromServer = reader.ReadToEnd();
-            
-            {
-                
-            }
-            
-            // string url = "https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&appid=d6bfd60ae10dc578300a860f105ed749"; // d6bfd60ae10dc578300a860f105ed749 - API-ключ
-
-            // string url = "https://api.openweathermap.org/data/2.5/weather?q=Vitebsk&units=metric&appid=f1451f839fdb9f6c9c04a07f128795ec";
-            // string url = "https://api.openweathermap.org/data/2.5/weather?q=Novopolotsk&units=metric&appid=8eedcba634b7c5a1f7fa0261db5cf635";
-            // string url = "https://api.openweathermap.org/data/2.5/weather?q=Brestk&units=metric&appid=55dca5e21670cdbf9b77d287b4965ab6";
-            // string url = "https://api.openweathermap.org/data/2.5/weather?q=Grodnok&units=metric&appid=a7eaf17a7cf263d162e40c802f229cd7";
-            
-            // HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url); // создала объект Request, чтобы сделать запрос
-            // HttpWebResponse httpWebResponse = (HttpWebResponse)HttpWebRequest.GetResponse(); // создала объект Response, чтобы считать данные с Request (get - возвращает)
-            // string response;
-            // using (StreamReader streamReader = new StreamReader(HttpWebResponse.GetResponseStream)) // считала все данные с httpWebResponse
-            // {
-                // response = streamReader.ReadToEnd();
-            // }
 
             // WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
             
@@ -54,11 +33,8 @@ namespace Weather_C_2
 
 // units=metric - температура в Цельсиях
 
-// enum Cities
-// {
-    // Minsk,
-    // Vitebsk,
-    // Novopolotsk,    
-    // Brest,
-    // Grodno
-// }
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&appid=d6bfd60ae10dc578300a860f105ed749";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Vitebsk&units=metric&appid=f1451f839fdb9f6c9c04a07f128795ec";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Novopolotsk&units=metric&appid=8eedcba634b7c5a1f7fa0261db5cf635";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Brestk&units=metric&appid=55dca5e21670cdbf9b77d287b4965ab6";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Grodnok&units=metric&appid=a7eaf17a7cf263d162e40c802f229cd7";
