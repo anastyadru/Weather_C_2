77b3814dc814d15f94e83b4e027812bded0e78ee
anastya_dru <adruts@mail.ru>
2023-02-19 23:27:36 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Weather.cs
+++ Weather_C_2/Weather.cs
@@ -4,6 +4,10 @@ namespace Weather_C_2
 {
     public class Weather
     {
-        
+        public double Temp { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
     }
 }
