27169f637a47ea3a0242bbb16663bbd7638f8b05
anastya_dru <adruts@mail.ru>
2023-02-21 13:48:03 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Program.cs
+++ Weather_C_2/Program.cs
@@ -21,7 +21,7 @@ namespace Weather_C_2
                 
                 var httpClient = new HttpClient(); // создала экземпляра HTTP-клиента
             
-                string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=1a5442d5206cd9ef2bfc21e5ca523b75";
+                string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=ru&appid=1a5442d5206cd9ef2bfc21e5ca523b75";
 
                 WebRequest request = WebRequest.Create(url); // отправила запрос и получила ответ
                 WebResponse response = request.GetResponse();
@@ -60,11 +60,11 @@ namespace Weather_C_2
 
 // units=metric - температура в Цельсиях
 
-// string url = "https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&appid=d6bfd60ae10dc578300a860f105ed749";
-// string url = "https://api.openweathermap.org/data/2.5/weather?q=Vitebsk&units=metric&appid=f1451f839fdb9f6c9c04a07f128795ec";
-// string url = "https://api.openweathermap.org/data/2.5/weather?q=Novopolotsk&units=metric&appid=8eedcba634b7c5a1f7fa0261db5cf635";
-// string url = "https://api.openweathermap.org/data/2.5/weather?q=Brest&units=metric&appid=55dca5e21670cdbf9b77d287b4965ab6";
-// string url = "https://api.openweathermap.org/data/2.5/weather?q=Grodno&units=metric&appid=a7eaf17a7cf263d162e40c802f229cd7";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&lang=ru&appid=d6bfd60ae10dc578300a860f105ed749";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Vitebsk&units=metric&lang=ru&appid=f1451f839fdb9f6c9c04a07f128795ec";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Novopolotsk&units=metric&lang=ru&appid=8eedcba634b7c5a1f7fa0261db5cf635";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Brest&units=metric&lang=ru&appid=55dca5e21670cdbf9b77d287b4965ab6";
+// string url = "https://api.openweathermap.org/data/2.5/weather?q=Grodno&units=metric&lang=ru&appid=a7eaf17a7cf263d162e40c802f229cd7";
 
 // GetForecastAsync(Cities.Vitebsk).Wait();
 // Console.WriteLine("Погода в городе Витебск:");
