ed05df764cc67b13a00228eead6d370228d2178e
anastya_dru <adruts@mail.ru>
2023-02-23 18:52:22 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Weather.cs
+++ Weather_C_2/Weather.cs
@@ -7,20 +7,26 @@ namespace Weather_C_2
 {
     public class Weather
     {
-        private double Temp { get; set; }
-        private int Pressure { get; set; }
-        private int Humidity { get; set; }
-        
-        public Weather(double temp, int pressure, int humidity)
-        {
-            Temp = temp;
-            Pressure = pressure;
-            Humidity = humidity;
-        }
-        
-        protected virtual void Print()
-        {
-            Console.WriteLine("Температура: {0}\nДавление: {1}\nВлажность: {2}%");
-        }
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
     }
 }
+
+    // {
+        // private double Temp { get; set; }
+        // private int Pressure { get; set; }
+        // private int Humidity { get; set; }
+    // }
+
+// public Weather(double temp, int pressure, int humidity)
+        // {
+            // Temp = temp;
+            // Pressure = pressure;
+            // Humidity = humidity;
+        // }
+        
+        // protected virtual void Print()
+        // {
+            // Console.WriteLine("Температура: {0}\nДавление: {1}\nВлажность: {2}%");
+        // }
