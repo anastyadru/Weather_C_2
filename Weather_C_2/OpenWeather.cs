758027485e7e6c3370170f0fe9a3e3cab837353f
anastya_dru <adruts@mail.ru>
2023-02-24 00:26:30 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/OpenWeather.cs
+++ Weather_C_2/OpenWeather.cs
@@ -10,9 +10,13 @@ namespace Weather_C_2
 {
     public class OpenWeather
     {
+        public Coordinates Coord { get; set; }
+        public List<Weather> Weather { get; set; }
         [JsonProperty("base")]
         public string Base { get; set; }
+        public Main Main { get; set; }
         public int Visibility { get; set; }
+        public Wind Wind { get; set; }
         public int Dt { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
