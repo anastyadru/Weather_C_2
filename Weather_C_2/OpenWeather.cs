0fd1799b7515eb9fc9ae206cf4826f38b4610f8b
anastya_dru <adruts@mail.ru>
2023-02-23 18:37:35 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/OpenWeather.cs
+++ Weather_C_2/OpenWeather.cs
@@ -5,11 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-
 namespace Weather_C_2
 {
     public class OpenWeather
     {
-        
+        [JsonProperty("base")]
+        public string Base { get; set; }
+        public int Visibility { get; set; }
+        public int Dt { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cod { get; set; }
     }
 }
