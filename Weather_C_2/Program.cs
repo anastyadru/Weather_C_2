d7c07e79c2dcddfb856bfd985cfe3043080b66d1
anastya_dru <adruts@mail.ru>
2023-03-01 03:45:00 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Program.cs
+++ Weather_C_2/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Weather_C_2
 {
@@ -12,7 +13,14 @@ namespace Weather_C_2
     {
         public static async Task GetWeatherDataAsync(string city)
         {
-            string url = "https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&lang=ru&appid=1a5442d5206cd9ef2bfc21e5ca523b75";
+            var client = new HttpClient(); // создала экземпляра HTTP-клиента
+            
+            var api_key = "1a5442d5206cd9ef2bfc21e5ca523b75";
+
+            Console.WriteLine("Прогноз погоды на 5 дней для города: ");
+            var city_name = Console.ReadLine().ToLower();
+
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric&lang=ru";
 
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url); // отправила запрос и получила ответ
             HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
@@ -43,11 +51,6 @@ namespace Weather_C_2
             try
             {
 
-                using (var client = new HttpClient()) // создала экземпляра HTTP-клиента
-                {
-                    
-                }
-                
                 switch (cityNumber)
                 {
                     case 1: 
