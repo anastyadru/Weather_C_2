7f6acfd557dac1e035cedb5d01ad02064889bc60
anastya_dru <adruts@mail.ru>
2023-03-02 12:00:38 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Program.cs
+++ Weather_C_2/Program.cs
@@ -11,22 +11,6 @@ namespace Weather_C_2
 {
     class Program
     {
-        public static async Task GetWeatherDataAsync(string city)
-        {
-            Console.WriteLine("Прогноз погоды на 5 дней для города: ");
-            var city_name = Console.ReadLine().ToLower();
-            var api_key = "1a5442d5206cd9ef2bfc21e5ca523b75";
-            
-            
-            var client = new HttpClient(); // создала экземпляра HTTP-клиента
-            
-            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric&lang=ru";
-
-            
-
-            Console.WriteLine();
-            Console.ReadLine();
-        }
 
         static void Main(string[] args)
         {
---
+++ Weather_C_2/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Weather_C_2
+{
+    class Task
+    {
+        public static async Task GetWeatherDataAsync(string city)
+        {
+            Console.WriteLine("Прогноз погоды на 5 дней для города: ");
+            var city_name = Console.ReadLine().ToLower();
+            var api_key = "1a5442d5206cd9ef2bfc21e5ca523b75";
+
+
+            var client = new HttpClient(); // создала экземпляра HTTP-клиента
+
+            var url =
+                $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric&lang=ru";
+
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
