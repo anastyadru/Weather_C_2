892c8fe13c97556a8bb4dc1d12a0889270b3f68b
anastya_dru <adruts@mail.ru>
2023-03-03 14:22:13 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Information.cs
+++ Weather_C_2/Information.cs
@@ -15,15 +15,13 @@ namespace Weather_C_2
         
         public static async Task<OpenWeather> GetWeather(string city)
         {
-            Console.WriteLine("Прогноз погоды на 5 дней для города: ");
-            var city_name = Console.ReadLine().ToLower();
-            var api_key = "1a5442d5206cd9ef2bfc21e5ca523b75";
-
-
+            var cityName = Console.ReadLine().ToLower();
+            var apiKey = "your_api_key_here"; // 1a5442d5206cd9ef2bfc21e5ca523b75
+            
             var client = new HttpClient(); // создала экземпляра HTTP-клиента
+            HttpResponseMessage response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric&lang=ru");
 
-            var url =
-                $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric&lang=ru";
+            
             
             Console.WriteLine();
             Console.ReadLine();
