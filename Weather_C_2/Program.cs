a4a45a956a9151d929a97d7f2c871371c8a2cba0
anastya_dru <adruts@mail.ru>
2023-03-13 16:42:08 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Program.cs
+++ Weather_C_2/Program.cs
@@ -9,62 +9,57 @@ namespace Weather_C_2
         static void Main(string[] args)
         {
             Console.WriteLine("Прогноз погоды на 5 дней для города: \n1 - Minsk \n2 - London \n3 - Paris \n4 - NewYork \n5 - Warsaw");
-
-            string newUrl;
-            string url;
-
+            
             try
             {
                 var cityNumber = int.Parse(Console.ReadLine());
                 City selectedCity = (City)(cityNumber - 1);
                 
+                string url = "";
                 switch (selectedCity)
                 {
                     case City.Minsk:
-                        var newUrl = "https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=d6bfd60ae10dc578300a860f105ed749&units=metric&lang=ru";
-                        Console.WriteLine("Погода в городе Минск: ");
-                        Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
-                        Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
-                        Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
-                        Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+                        var url = "https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=d6bfd60ae10dc578300a860f105ed749&units=metric&lang=ru";
                         break;
 
                     case City.London:
-                        var newUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=f1451f839fdb9f6c9c04a07f128795ec&units=metric&lang=ru";
-                        Console.WriteLine("Погода в городе Лондон: ");
-                        Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
-                        Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
-                        Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
-                        Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+                        var url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=f1451f839fdb9f6c9c04a07f128795ec&units=metric&lang=ru";
                         break;
 
                     case City.Paris:
-                        var newUrl = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=8eedcba634b7c5a1f7fa0261db5cf635&units=metric&lang=ru";
-                        Console.WriteLine("Погода в городе Париж: ");
-                        Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
-                        Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
-                        Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
-                        Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+                        var url = "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=8eedcba634b7c5a1f7fa0261db5cf635&units=metric&lang=ru";
                         break;
 
                     case City.NewYork:
-                        var newUrl = "https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=55dca5e21670cdbf9b77d287b4965ab6&units=metric&lang=ru";
-                        Console.WriteLine("Погода в городе Нью-Йорк: ");
-                        Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
-                        Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
-                        Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
-                        Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+                        var url = "https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=55dca5e21670cdbf9b77d287b4965ab6&units=metric&lang=ru";
                         break;
 
                     case City.Warsaw:
-                        var newUrl = "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=a7eaf17a7cf263d162e40c802f229cd7&units=metric&lang=ru";
-                        Console.WriteLine("Погода в городе Варшава: ");
-                        Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
-                        Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
-                        Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
-                        Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+                        var url = "https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=a7eaf17a7cf263d162e40c802f229cd7&units=metric&lang=ru";
                         break;
                 }
+                
+                string result = "";
+                using (var client = new HttpClient())
+                {
+                    var response = client.GetAsync(url).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = response.Content.ReadAsStringAsync().Result;
+                        var weatherData = JsonConvert.DeserializeObject<WeatherData>(content);
+                        result += $"Погода в городе {cityName}: \n";
+                        result += $"Температура: {weatherData.Data.Temp}°C\n";
+                        result += $"Температура ощущается на: {weatherData.Data.FeelsLike}°C\n";
+                        result += $"Давление: {weatherData.Data.Pressure}Pa\n";
+                        result += $"Влажность: {weatherData.Data.Humidity}%\n";
+                    }
+                    else
+                    {
+                        result += $"Ошибка получения данных о погоде в городе {cityName}\n";
+                    }
+                }
+
+                return result;
             }
 
             catch (Exception ex)
@@ -86,3 +81,34 @@ namespace Weather_C_2
         }
     }
 }
+
+// Console.WriteLine("Погода в городе Минск: ");
+// Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
+// Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
+// Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
+// Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+
+// Console.WriteLine("Погода в городе Лондон: ");
+// Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
+// Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
+// Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
+// Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+
+// Console.WriteLine("Погода в городе Париж: ");
+// Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
+// Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
+// Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
+// Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+
+// Console.WriteLine("Погода в городе Нью-Йорк: ");
+// Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
+// Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
+// Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
+// Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+
+// Console.WriteLine("Погода в городе Варшава: ");
+// Console.WriteLine($"Температура: {WeatherData.Data.Temp}°C");
+// Console.WriteLine($"Температура ощущается на: {WeatherData.Data.FeelsLike}°C");
+// Console.WriteLine($"Давление: {WeatherData.Data.Pressure}Pa");
+// Console.WriteLine($"Влажность: {WeatherData.Data.Humidity}%");
+
