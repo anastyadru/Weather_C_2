37285d0a8bbb4c2a38f4727a65d6f3849753bc69
Nastia <adruts@mail.ru>
2023-05-17 00:19:32 +0300

    https://github.com/anastyadru/Weather_C_2.git

--- Weather_C_2/Program.cs
+++ Weather_C_2/Program.cs
@@ -7,7 +7,7 @@ namespace Weather_C_2
 {
     public class Program
     {
-        
+
         static async Task Main(string[] args)
         {
             string url;
@@ -16,6 +16,7 @@ namespace Weather_C_2
             try
             {
                 Console.WriteLine("Введите, для какого города прогноз погоды на 5 дней: Minsk, London, Paris, NewYork, Warsaw");
+                
                 cityName = Console.ReadLine();
                 url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=d6bfd60ae10dc578300a860f105ed749&units=metric&lang=ru";
             }
@@ -23,40 +24,36 @@ namespace Weather_C_2
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
-                Console.WriteLine ("Такого города не существует в заготовленном списке. Введите город вручную: ");
+                Console.WriteLine("Такого города не существует в заготовленном списке. Введите город вручную: ");
+                
                 cityName = Console.ReadLine();
                 url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=1a5442d5206cd9ef2bfc21e5ca523b75&units=metric&lang=ru";
             }
-            
+
             var information = new Information();
-            var weatherData = await information.PrintAsync(url);
-            
+            var weatherData = await information.PrintAsync(cityName);
+
             var result = "";
 
-            using (var client = new HttpClient())
+            if (weatherData != null)
             {
-                var response = client.GetAsync(url).Result;
-                if (weatherData != null)
-                {
-                    var responseBody = response.Content.ReadAsStringAsync().Result;
-                    // WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(responseBody);
-                    result += $"Погода в городе {WeatherData.City}: \n";
-                    result += $"Температура: {WeatherData.Data.Temp}°C\n";
-                    result += $"Температура ощущается на: {WeatherData.Data.FeelsLike}°C\n";
-                    result += $"Давление: {WeatherData.Data.Pressure}Pa\n";
-                    result += $"Влажность: {WeatherData.Data.Humidity}%\n";
-                }
-                
-                else
-                {
-                    result += $"Ошибка получения данных о погоде в городе {cityName}\n";
-                }
+                result += $"Погода в городе {WeatherData.City}: \n";
+                result += $"Температура: {WeatherData.Data.Temp}°C\n";
+                result += $"Температура ощущается на: {WeatherData.Data.FeelsLike}°C\n";
+                result += $"Давление: {WeatherData.Data.Pressure}Pa\n";
+                result += $"Влажность: {WeatherData.Data.Humidity}%\n";
             }
-            
+
+            else
+            {
+                result += $"Ошибка получения данных о погоде в городе {cityName}\n";
+            }
+
             Console.WriteLine(result);
             Console.ReadLine();
         }
         
+
     }
 }
 
